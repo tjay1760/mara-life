8985eb186f6305ef4aafb369aeda37a0fd90abce
tjay1760 <mcjthiongo@gmail.com>
2024-12-16 14:29:57 +0300

    fixed eslint error

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+    </div>
   )
 }
 

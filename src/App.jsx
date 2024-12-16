c58c8022930cba59269398d3f57f594bba8b87f4
tjay1760 <mcjthiongo@gmail.com>
2024-12-16 16:26:29 +0300

    floated responsive menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar';
 import HomePage from './components/Home/HomePage';
+import AboutPage from './components/About/AboutPage';
+import TrainingsPage from './components/Trainings/TrainingsPage';
+import BlogPage from './components/Blog/BlogPage';
+import ProductsPage from './components/Products/ProductsPage';
+import ContactsPage from './components/Contacts/ContactPage';
+
 import {Routes, Route } from "react-router";
-import {}
 
 
 const App = () => {
@@ -10,10 +15,15 @@ const App = () => {
     <>
       <Navbar/>
       <Routes>
-      <HomePage/>
+      <Route path ={"/"} element ={<HomePage/>}/>
+      <Route path ={"/contact"} element ={<ContactsPage/>}/>
+      <Route path ={"/about"} element ={<AboutPage/>}/>
+      <Route path ={"/blog"} element ={<BlogPage/>}/>
+      <Route path ={"/products"} element ={<ProductsPage/>}/>
+      <Route path ={"/trainings"} element ={<TrainingsPage/>}/>
       </Routes>
       
-    <>
+    </>
   )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router";
+import NavLogo from '../../assets/navbar/Navbar_logo.svg'
 const Navbar = () => {
   return (
-    <div>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-    </div>
-  )
-}
+    <div className="flex flex-col justify-center items-center border border-gray-300 rounded-2xl h-96 m-3 gap-3">
+        <img src={NavLogo} className="pb-6 px-4"/>
+        <nav className="flex flex-col gap-4 font-semibold text-gray-500 text-center">
+        <Link to={"/"}>
+        <button>Home</button>
+      </Link>
+      <Link to={"/about"}>
+        <button>About</button>
+      </Link>
+      <Link to={"/products"}>
+        <button>Product</button>
+      </Link>
+      <Link to={"/trainings"}>
+        <button>Trainings</button>
+      </Link>
+      <Link to={"/blog"}>
+        <button>Blog</button>
+      </Link>
+      <Link to={"/contact"}>
+        <button>Contact</button>
+      </Link>
+        </nav>
 
-export default Navbar
+    </div>
+  );
+};
+
+export default Navbar;

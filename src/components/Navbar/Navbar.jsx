5a9eab68baa7dd047f4aa7a806088ee5b96993c2
tjay1760 <mcjthiongo@gmail.com>
2024-12-16 18:56:18 +0300

    navigation menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,45 +13,42 @@ const Navbar = () => {
   };
 
   return (
-    <div className="flex flex-col items-center border border-gray-300 rounded-2xl h-96 m-3 gap-3">
-      {/* Logo */}
-      <img src={NavLogo} className="pb-6 px-4" alt="Navbar Logo" />
-
-      {/* Menu Toggle Button */}
-      <button
-        onClick={toggleMenu}
-        className="text-2xl focus:outline-none md:hidden"
-        aria-label="Toggle menu"
-      >
-        {isMenuOpen ? <IoCloseOutline /> : <IoMenuOutline />}
-      </button>
-
-      {/* Navigation Menu */}
-      <nav
-        className={`${
-          isMenuOpen ? "flex" : "hidden"
-        } flex-col gap-4 font-semibold text-gray-500 text-center md:flex`}
-      >
-        <Link to={"/"}>
-          <button>Home</button>
-        </Link>
-        <Link to={"/about"}>
-          <button>About</button>
-        </Link>
-        <Link to={"/products"}>
-          <button>Product</button>
-        </Link>
-        <Link to={"/trainings"}>
-          <button>Trainings</button>
-        </Link>
-        <Link to={"/blog"}>
-          <button>Blog</button>
-        </Link>
-        <Link to={"/contact"}>
-          <button>Contact</button>
-        </Link>
-      </nav>
-    </div>
+    <>
+    <button
+       onClick={toggleMenu}
+       className={`${isMenuOpen?"border-none": "border border-gray-200 rounded-xl p-[1px]"} text-3xl text-gray-600 md:hidden absolute top-4 right-2`}
+       aria-label="Toggle menu"
+     >
+       {isMenuOpen ? <IoCloseOutline /> : <IoMenuOutline />}
+     </button>
+    <div className={`${isMenuOpen ? "flex" : "hidden"} flex-col items-center border border-gray-300 rounded-2xl h-96 m-3 gap-3`}>
+     
+     <img src={NavLogo} className="pb-6 px-4 pt-3 h-20" alt="Navbar Logo" />
+     <nav
+       className={" flex flex-col gap-4 font-semibold text-gray-500 text-center md:flex"}
+     >
+       <Link to={"/"}>
+         <button>Home</button>
+       </Link>
+       <Link to={"/about"}>
+         <button>About</button>
+       </Link>
+       <Link to={"/products"}>
+         <button>Product</button>
+       </Link>
+       <Link to={"/trainings"}>
+         <button>Trainings</button>
+       </Link>
+       <Link to={"/blog"}>
+         <button>Blog</button>
+       </Link>
+       <Link to={"/contact"}>
+         <button>Contact</button>
+       </Link>
+     </nav>
+   </div>
+    </>
+    
   );
 };
 

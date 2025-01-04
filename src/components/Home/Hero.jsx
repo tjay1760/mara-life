2f41f795268789edb56e27e3e2f5f859abde27ac
tjay1760 <mcjthiongo@gmail.com>
2025-01-04 13:05:13 +0300

    refined styling

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
 <>
 <div className="hero-container relative">
-    <div className="cut-out h-12 w-12 bg-white absolute right-0 rounded-xl md:w-[63%] md:h-16"></div>
+    <div className="cut-out h-12 w-12 bg-white absolute right-0 rounded-xl md:w-[62%] md:h-[3.8rem]"></div>
     <div className="hero-holder bg-green-950 h-lvh mx-auto mt-3 w-11/12 border rounded-3xl md:w-[98%]"></div>
 </div>
 </>

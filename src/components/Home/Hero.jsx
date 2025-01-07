0df6465c5020091f276a3908f120a72fbadcb3d7
tjay1760 <mcjthiongo@gmail.com>
2025-01-07 16:07:56 +0300

    hero pending navigation fixes

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -20,25 +20,25 @@ const Hero = () => {
     <>
       <div className="hero-container relative h-auto">
         <div className="cut-out h-12 w-12 bg-white absolute right-0 rounded-xl md:w-[62%] md:h-[3.8rem]"></div>
-        <div className="hero-holder h-lvh mx-auto mt-3 w-11/12 border rounded-3xl md:w-[98%]"
+        <div className="hero-holder mx-auto mt-3 w-11/12 border rounded-3xl md:w-[98%]"
         style={{
             backgroundImage: `url(${heroBackground})`,
             backgroundSize: "cover",
             backgroundPosition: "center",
           }}
         >
-          <div className="logo mt-3 mx-4 w-28">
+          <div className="logo mt-3 mx-4 w-28 md:w-40">
             <img src={Logo} />
           </div>
-          <div className="header-writting text-white font-bold text-4xl p-2">
+          <div className="header-writting text-white font-bold text-4xl p-2 md:text-7xl md:w-1/3 md:mt-10 md:mx-10">
             Training on Sustainable Farming
           </div>
-          <div className="header-description glass m-2 p-2">
+          <div className="header-description glass m-2 p-2 md:mx-10 md:mt-10 md:w-1/2">
             Welcome to Maralife your trusted partner in sustainable farming. We
             provide top-quality pesticide products to protect your crops and
             maximize yields.
           </div>
-          <div className="font-bold mt-2 mx-auto header-buttons text-gray-700 bg-white w-11/12 border border-gray-900 p-2 rounded-2xl">
+          <div className="font-bold mt-2 mx-auto md:mx-10 md:mt-10 header-buttons text-gray-700 bg-white w-[18rem] border border-gray-900 p-2 rounded-2xl">
             <button className="bg-white rounded-lg mx-1 hover:bg-gray-300 active:bg-green-600 p-2">
               Register Now
             </button>
@@ -62,7 +62,7 @@ const Hero = () => {
                   },
                }}
                onSlideChange={(swiper) => {
-                // Update background image on slide change
+
                 setHeroBackground(backgroundImages[swiper.activeIndex]);
               }}
             >

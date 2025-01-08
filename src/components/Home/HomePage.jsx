ce6b928ee9eac8912b04634deb4803a87db69605
tjay1760 <mcjthiongo@gmail.com>
2025-01-08 04:44:19 +0300

    who we are mobile

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './Hero'
+import WhoWeAre from './WhoWeAre'
 
 const HomePage = () => {
   return (
     <>
     <Hero/>
+    <WhoWeAre/>
     </>
     
   )

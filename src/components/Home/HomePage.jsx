1a715df4245aa25d347dc71b1688a406a355d068
tjay1760 <mcjthiongo@gmail.com>
2025-01-08 05:16:24 +0300

    added products

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './Hero'
 import WhoWeAre from './WhoWeAre'
+import Products from './Products'
 
 const HomePage = () => {
   return (
     <>
     <Hero/>
     <WhoWeAre/>
+    <Products/>
     </>
     
   )

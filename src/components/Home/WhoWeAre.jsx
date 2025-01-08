1d03d738a08968603df8152a52c26c28422b8059
tjay1760 <mcjthiongo@gmail.com>
2025-01-08 04:50:57 +0300

    who we are done

--- src/components/Home/WhoWeAre.jsx
+++ src/components/Home/WhoWeAre.jsx
@@ -3,8 +3,9 @@ import LabPhoto from "../../assets/who-we-are/an image of two african scientists
 const WhoWeAre = () => {
   return (
     <>
-      <div className="who-we-are mt-10 border border-red-500 w-11/12 mx-auto">
-        <h1 className="text-3xl font-bold text-green-900">Who we are</h1>
+      <div className="who-we-are mt-10  w-11/12 mx-auto md:flex gap-2 p-2">
+      <div className="who-we-are-titles">
+      <h1 className="text-3xl font-bold text-green-900">Who we are</h1>
         <p className="statement mt-3">
           Mara Lifesciences Ltd is a biopesticides and biofertilizers production
           company based in Nairobi, Kenya. We specialize in developing
@@ -13,7 +14,8 @@ const WhoWeAre = () => {
           farmers with effective, environmentally conscious products while
           offering technical support and training.
         </p>
-        <img className ="mt-3" src={LabPhoto}/>
+      </div>
+        <img className ="mt-3 md:w-1/2 rounded-xl" src={LabPhoto}/>
       </div>
     </>
   );

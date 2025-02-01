b9f12ef66bd28fd9030f2130707dfd24cb1839eb
tjay1760 <mcjthiongo@gmail.com>
2025-02-01 13:51:26 +0300

    structure

--- src/components/Products/RegistrationForm.jsx
+++ src/components/Products/RegistrationForm.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 const RegistrationForm = () => {
+  const { register, handleSubmit, formState:{errors} } = useForm();
+  const onSubmit = (data) => {        
+    console.log(data);
+  }
   return (
-    <div>RegistrationForm</div>
+    <div className='mt-10 bg-gray-100 w-11/12 mx-auto p-2 flex flex-col items-center justify-center text-center'>
+      
+      <h1 className='text-green-950 font-bold text-2xl'>Pesticide Registration Assistance Form</h1>
+      <p>Please fill out the form below to register your pesticide product with our assistance program.</p>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-holder">
+          <button>
+            
+          </button>
+        </div>
+      </form>
+    </div>
   )
 }
 

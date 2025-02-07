577d2b35894456c377b1ee908dab6622a01fd30d
tjay1760 <mcjthiongo@gmail.com>
2025-02-07 07:56:58 +0300

    add Descriptions component to AboutPage for enhanced company information display

--- src/components/About/AboutPage.jsx
+++ src/components/About/AboutPage.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Hero from './Hero'
 import CompanyBackground from './CompanyBackground'
-
+import Descriptions from './Descriptions'
 const AboutPage = () => {
   return (
     <div>
 <Hero/>
 <CompanyBackground/>
+<Descriptions/>
     </div>
   )
 }
---
+++ src/components/About/Descriptions.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import coreValuesimg from '../../assets/about/core-values.png'
+import missionimg from '../../assets/about/mission.png'
+import certificationimg from '../../assets/about/certifications.png'
+
+const values = [
+    {
+        title: 'Core Values:',
+        img: coreValuesimg,
+        description: 'Core Values: Customer focus, speed, and agility.'
+    },
+    {
+        title: 'Mission & Vision:',
+        img: missionimg,
+        description: 'Mission & Vision: From seed to harvest, we leverage technology, integrating genetics, quality-focused practices, and IPM strategies, to provide you with a complete solution.'
+    },
+    {
+        title: 'Certifications:',
+        img: certificationimg,
+        description: 'Certifications: We implement GLP and HACCP practices in our laboratory.'
+    }
+]
+const Descriptions = () => {
+  return (
+    <div className='descriptions-holder bg-gray-300'>
+        {values.map((value, index) => (
+            <div key={index} className='description border flex flex-col items-center gap-2 p-2'>
+                <h1 className='text-3xl font-bold text-green-900'>{value.title}</h1>
+                <img src={value.img} className='h-20'/>
+                <p className='statement mt-3 text-center'>{value.description}</p>
+            </div>
+        ))}
+    
+    
+    
+    </div>
+  )
+}
+
+export default Descriptions    

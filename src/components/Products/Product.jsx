18c71c7cd45854a94c9b6deaaee3b43a2bd7b83b
tjay1760 <mcjthiongo@gmail.com>
2025-02-07 22:36:35 +0300

    revert path

--- src/components/Products/Product.jsx
+++ src/components/Products/Product.jsx
@@ -12,7 +12,7 @@ const Product = () => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch("s/products.json")
+    fetch("/products.json")
       .then((response) => response.json())
       .then((data) => {
         console.log(data)

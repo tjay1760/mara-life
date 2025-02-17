bbc186cc995eb7f025651b765fe30af21b1efd74
tjay1760 <mcjthiongo@gmail.com>
2025-02-17 12:10:18 +0300

    added partners to trainings page

--- src/components/Trainings/TrainingsPage.jsx
+++ src/components/Trainings/TrainingsPage.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
+import Partners from '../Home/Partners'
 
 const TrainingsPage = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-200 w-11/12 mx-auto rounded-xl p-4">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold text-green-800">Coming Soon</h1>
-        <p className="text-lg text-gray-600 mt-2">Our Trainings page is under construction. Stay tuned for updates!</p>
-      </div>
+    <div className="trainings-page">
+      <Partners />
     </div>
   )
 }

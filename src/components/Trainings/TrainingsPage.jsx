de612b2d6b0c67d851774dd1835150944c1c0705
tjay1760 <mcjthiongo@gmail.com>
2025-02-17 10:26:21 +0300

    added default trainings page

--- src/components/Trainings/TrainingsPage.jsx
+++ src/components/Trainings/TrainingsPage.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const TrainingsPage = () => {
   return (
-    <div>TrainingsPage</div>
+    <div className="flex items-center justify-center h-screen bg-gray-200 w-11/12 mx-auto rounded-xl p-4">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-green-800">Coming Soon</h1>
+        <p className="text-lg text-gray-600 mt-2">Our Trainings page is under construction. Stay tuned for updates!</p>
+      </div>
+    </div>
   )
 }
 

589181ded252861a4e66e61c854feb171a97dd2a
tjay1760 <mcjthiongo@gmail.com>
2025-02-18 10:11:41 +0300

    refactor: update navigation in Hero component and enhance Descriptions layout

--- src/components/Trainings/Descriptions.jsx
+++ src/components/Trainings/Descriptions.jsx
@@ -6,12 +6,12 @@ import enterprisesIcon from "../../assets/products/enterprises-icon.svg";
 
 const Descriptions = () => {
   return (
-    <div className="w-11/12 mx-auto mt-10 p-2">
+    <div className="w-11/12 mx-auto mt-10 p-2 md:grid md:grid-cols-2 md:gap-5">
         <div className="descriptions flex flex-col gap-2">
             <h1 className='text-green-950 font-bold text-4xl'>We empower excellence through exemplary trainings and consulting</h1>
             <p>Our expert training and consulting solutions pave way for your organisation or individual success.</p>
-            <button className='bg-gray-300 py-1 px-2 rounded-lg'>Register Now</button>
-            <div className="companies flex border p-2 gap-2 rounded-lg text-xs">
+            <button className='bg-gray-300 py-1 px-2 rounded-lg md:max-w-fit'>Register Now</button>
+            <div className="companies flex border p-2 gap-2 rounded-lg text-xs md:text-sm md:justify-between md:mt-20">
               <div className="individuals flex gap-1">
                 <img src={individualIcon} className='h-4'/>
                 <span>Individuals</span>
